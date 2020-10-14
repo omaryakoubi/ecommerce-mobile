8ec2ee18bbd7daee31781ca69579df3be35e3de8
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-14 15:58:18 +0100

    fixed

--- src/scenes/ecommerce/product-list.component.tsx
+++ src/scenes/ecommerce/product-list.component.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 import { SafeAreaLayout } from '../../components/safe-area-layout.component';
-import { ArrowIosBackIcon } from '../../components/icons';
 import ContentView from '../../layouts/ecommerce/product-list';
 
 export const ProductListScreen = ({ navigation }): React.ReactElement => {
